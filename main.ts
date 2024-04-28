198ac7ed0683b966778d838b4978a99803c5fa8d
Iqra naz <162154619+Iqranaz993@users.noreply.github.com>
2024-04-28 19:45:58 +0500

    This is my 04 todo list app assignment

---
+++ main.ts
@@ -0,0 +1,83 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+console.log("Welcome to my todo list app!")
+
+let toDos: string[] = ["iqra" , "hania" , "hajwa"];
+let condition = true;
+
+ while(condition){
+            let answers = await inquirer.prompt({
+            name : "operator",
+            type: "list",
+            message: "select an operator:",
+            choices: ["Add" , "Update" , "View" , "Delete"]
+
+});
+
+  if (answers.operator == "Add"){
+    let addTodo = await inquirer.prompt(
+      [
+        {
+        name : "add",
+        message: "what do you want to add your todo?",
+        type : "input"
+       
+    }
+      ] 
+          );
+    toDos.push(addTodo.add);
+    condition= addTodo.add;
+    console.log(toDos)
+
+  }
+
+  if (answers.operator == "Update"){
+        let updateTodo = await inquirer.prompt(
+        [
+          {
+            name :   "update",
+            message: "What do you want to Update in your list?",
+            type :   "list",
+            choices: toDos.map(item => item)
+          }
+        ]
+            );
+
+        let addTodo = await inquirer.prompt(
+         [
+          {
+            name : "add",
+            message: "what do you want to add your list?",
+            type : "input"
+          }
+         ] 
+           );
+        let newTodo = toDos.filter(val =>val ! == updateTodo.add);
+        toDos = [...newTodo,addTodo.add];
+        console.log(toDos)
+
+   }
+
+  if (answers.operator == "View"){
+    console.log("****** ToDo list app ******");
+    console.log(toDos);
+
+  }
+  if (answers.operator == "Delete"){
+    let DeleteTodo = await inquirer.prompt([{
+        name :   "deleteTodo",
+        message: "What do you want to delete in your list?",
+        type :   "list",
+        choices: toDos.map(item => item)
+    }]);
+    let newTodo = toDos.filter(val =>val ! == DeleteTodo.add);
+    toDos = [...newTodo];
+    console.log(toDos);
+
+}
+    } 
+
+
